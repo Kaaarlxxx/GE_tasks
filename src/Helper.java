4923e3b8c0714e3b3cbe584b3b52611d40243d3c
Andrzej Melcer <andymelcer@wp.pl>
2021-05-24 20:39:17 +0100

    Refactor code

--- src/Helper.java
+++ src/Helper.java
@@ -4,24 +4,24 @@ import java.util.Random;
 
 public class Helper {
 
-    public static void printTable(String tab[]){
+    public static void printTable(String tab[]) {
 
-        for(int i = 0; i < tab.length; i++){
-            System.out.print(tab[i]+"   ");
+        for (int i = 0; i < tab.length; i++) {
+            System.out.print(tab[i] + "   ");
         }
 
         System.out.println();
     }
 
-    public static ArrayList reverseProb(ArrayList<Double> probs, int population_size){
+    public static ArrayList reverseProb(ArrayList<Double> probs, int population_size) {
         ArrayList<Double> newProbs = new ArrayList<Double>();
         for (int i = 0; i < population_size; i++) {
-            newProbs.add((1 - probs.get(i))/(population_size-1));
+            newProbs.add((1 - probs.get(i)) / (population_size - 1));
         }
         return newProbs;
     }
 
-    public static ArrayList readTasksFromFile(String fileName)  {
+    public static ArrayList readTasksFromFile(String fileName) {
         String[] val;
         Task tmp = new Task();
         ArrayList<Task> input = new ArrayList<Task>();
@@ -43,19 +43,19 @@ public class Helper {
                 count++;
             }
             br.close();
+        } catch (IOException e) {
         }
-        catch (IOException e){}
         return input;
     }
 
-    public static int posRoulette(ArrayList<Double> probs,int population_size){
+    public static int posRoulette(ArrayList<Double> probs, int population_size) {
         Random r = new Random();
         double posib = 0;
-        int pos=0;
+        int pos = 0;
         double rand = r.nextDouble();
         for (int i = 0; i < population_size; i++) {
-            posib +=probs.get(i);
-            if(rand<=posib){
+            posib += probs.get(i);
+            if (rand <= posib) {
                 pos = i;
                 break;
             }
@@ -63,19 +63,19 @@ public class Helper {
         return pos;
     }
 
-    public static ArrayList countProbs(ArrayList<Permutation> Population,int pop_size){
+    public static ArrayList countProbs(ArrayList<Permutation> Population, int pop_size) {
         ArrayList<Double> newProbs = new ArrayList<Double>();
         double sum = 0;
         int i = 0;
-        while(i<pop_size) sum += Population.get(i++).delay;
+        while (i < pop_size) sum += Population.get(i++).delay;
         for (int j = 0; j < pop_size; j++) {
-            newProbs.add(Population.get(j).delay/sum);
+            newProbs.add(Population.get(j).delay / sum);
         }
 
-        return Helper.reverseProb(newProbs,pop_size);
+        return Helper.reverseProb(newProbs, pop_size);
     }
 
-  // CountDelay4All todo
+    // CountDelay4All todo
 
 }
 
--- src/MinTaskDelay.java
+++ src/MinTaskDelay.java
@@ -1,7 +1,9 @@
-
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Random;
+
 import static java.util.Collections.shuffle;
 
 
@@ -12,107 +14,103 @@ public class MinTaskDelay {
 
         Random r = new Random();
         ArrayList<Task> input = Helper.readTasksFromFile("projekt8.txt");
-        ArrayList<Double> probs =new ArrayList<Double>();
-        ArrayList<Permutation> Population = new ArrayList<>();
-        ArrayList<Permutation> PopulationAfterRoulette = new ArrayList<>();
+        ArrayList<Double> probs = new ArrayList<Double>();
+        ArrayList<Permutation> population = new ArrayList<>();
+        ArrayList<Permutation> populationAfterRoulette = new ArrayList<>();
         int N = 200; //rozmiar populacji
-        Permutation BestPerm;
+        Permutation bestPerm;
         Permutation tmpBestPerm;
 
         PrintWriter highest = null;
         PrintWriter temp = null;
 
-         try{
-        highest = new PrintWriter(new FileOutputStream("highest.txt"));
-        temp = new PrintWriter(new FileOutputStream("tmp.txt"));
+        try {
+            highest = new PrintWriter(new FileOutputStream("highest.txt"));
+            temp = new PrintWriter(new FileOutputStream("tmp.txt"));
         } catch (FileNotFoundException e) {
-        e.printStackTrace();
-         }
-
-
+            e.printStackTrace();
+        }
         //wypelnianie populacji taskami w losowej kolejnosci
-        Permutation tmpperm = new Permutation(input,0);
+        Permutation tmpperm = new Permutation(input, 0);
         for (int i = 0; i < N; i++) {
             shuffle(tmpperm.tasks);
-            Population.add(tmpperm.copy());
-            Population.get(i).countDelay();
-            System.out.println(Population.get(i).delay);
+            population.add(tmpperm.copy());
+            population.get(i).countDelay();
+            System.out.println(population.get(i).delay);
         }
 
-        probs = Helper.countProbs(Population,N);
+        probs = Helper.countProbs(population, N);
 
 
 //       // just print for test
 //          for (int i = 0; i < N; i++) {
-//              tmpperm=Population.get(i);
+//              tmpperm=population.get(i);
 //              tmpperm.print();
 //        }
 
 
-    // wybieranie najlepszej permutacji (z najmniejszym opoznieniem)
-    BestPerm = Population.get(0).copy();
-    highest.println(BestPerm.delay);
+        // wybieranie najlepszej permutacji (z najmniejszym opoznieniem)
+        bestPerm = population.get(0).copy();
+        highest.println(bestPerm.delay);
 
-    for (int i = 1; i < N; i++) {
-        if(Population.get(i).delay < BestPerm.delay){
-            BestPerm = Population.get(i).copy();
-        }
-    }
-    double probability;
-
-    for (int ev = 0; ev < 1000; ev ++) {
-
-        //Mutowanie
-        for(int i = 0; i < N; i++) {
-            probability = r.nextDouble();
-            if(probability<0.2){
-                Population.get(i).mutate();
+        for (int i = 1; i < N; i++) {
+            if (population.get(i).delay < bestPerm.delay) {
+                bestPerm = population.get(i).copy();
             }
         }
+        double probability;
 
+        for (int ev = 0; ev < 1000; ev++) {
 
-        //PMX todo
-
-
-
-
-        //ruletka
-        for (int i = 0; i < N; i++) {
-            PopulationAfterRoulette.add(Population.get(Helper.posRoulette(probs,N)).copy());
-        }
-
-
-        Population = (ArrayList<Permutation>) PopulationAfterRoulette.clone();
-        PopulationAfterRoulette.clear();
-
-        for (int i = 0; i < N; i++) {
-            tmpperm = Population.get(i).copy();
-            tmpperm.countDelay();
-            Population.set(i,tmpperm);
-
-        }
-
-
-        probs = Helper.countProbs(Population,N);
-
-
-        tmpBestPerm = Population.get(0).copy();
-        for (int i = 0; i < N; i++) {
-            if(Population.get(i).delay < tmpBestPerm.delay){
-                tmpBestPerm = Population.get(i).copy();
+            //Mutowanie
+            for (int i = 0; i < N; i++) {
+                probability = r.nextDouble();
+                if (probability < 0.2) {
+                    population.get(i).mutate();
+                }
             }
+
+
+            //PMX todo
+            
+
+            //ruletka
+            for (int i = 0; i < N; i++) {
+                populationAfterRoulette.add(population.get(Helper.posRoulette(probs, N)).copy());
+            }
+
+
+            population = (ArrayList<Permutation>) populationAfterRoulette.clone();
+            populationAfterRoulette.clear();
+
+            for (int i = 0; i < N; i++) {
+                tmpperm = population.get(i).copy();
+                tmpperm.countDelay();
+                population.set(i, tmpperm);
+
+            }
+
+
+            probs = Helper.countProbs(population, N);
+
+
+            tmpBestPerm = population.get(0).copy();
+            for (int i = 0; i < N; i++) {
+                if (population.get(i).delay < tmpBestPerm.delay) {
+                    tmpBestPerm = population.get(i).copy();
+                }
+            }
+
+
+            if (tmpBestPerm.delay < bestPerm.delay) bestPerm = tmpBestPerm.copy();
+            temp.println(tmpBestPerm.delay);
+            highest.println(bestPerm.delay);
         }
-
-
-        if(tmpBestPerm.delay<BestPerm.delay) BestPerm = tmpBestPerm.copy();
-        temp.println(tmpBestPerm.delay);
-        highest.println(BestPerm.delay);
-    }
 
 
         highest.close();
         temp.close();
-        BestPerm.print();
+        bestPerm.print();
 
     }
 }
